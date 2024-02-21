a3d61dd2376261d59e5aff093ac8fdc1d0b8e5f7
Forrest Beck <forrest.beck@gmail.com>
2024-02-21 17:44:48 -0500

    while

--- main.py
+++ main.py
@@ -11,6 +11,7 @@ def print_hi(name):
 
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
-    print_hi('PyCharm Hey')
+    while True:
+        print_hi('PyCharm Hey')
 
 # See PyCharm help at https://www.jetbrains.com/help/pycharm/
